cf99ed568c83526fd622ceff83b381bfd9958a71
Raunak Pandey <raunakp636@gmail.com>
2025-06-23 11:33:05 +0530

    change

--- backend/middleware/uploadMiddleware.js
+++ backend/middleware/uploadMiddleware.js
@@ -2,6 +2,8 @@
 const multer = require('multer');
 const path = require('path');
 const fs = require('fs');
+const Course = require('../models/Course');
+
 const cloudinary = require('../utils/cloudinary');
 
 const createUploadDirs = () => {
